8ac75d05f3b663a0a0ff6ba88ab4f034814babad
PedroFilho <pedrohaidarfilho@gmail.com>
2022-06-08 22:51:31 -0400

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:crud/ui/contact_page.dart';
+import 'package:flutter/material.dart';
+
+import 'ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
